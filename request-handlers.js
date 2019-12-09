15072bd3363ef1e52525a5295d6892a6530f2947
Caleb Pendergraft <cackmed@gmail.com>
2019-12-08 17:32:00 -0800

    updated code

--- request-handlers.js
+++ request-handlers.js
@@ -1,12 +1,20 @@
+const exec = require('child_process').exec;
+
 function start() {
-    console.log("Request handler 'start' was called.");
-    return 'Hello Start';
+    console.log('Request handler' + start + 'was called.');
+    let content = 'empty';
+
+    exec('ls -lah', function(error, stdout, stderr) {
+        content = stdout;
+    });
+
+    return content;
 }
-    
+
 function upload() {
-    console.log("Request handler 'upload' was called.");
+    console.log('Request handler' + upload + 'was called.');
     return 'Hello Upload';
 }
-    
+
 exports.start = start;
 exports.upload = upload;
