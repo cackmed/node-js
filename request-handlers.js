96542b369215361a6ed9d99c2f4ffc5f03d84ad4
Caleb Pendergraft <cackmed@gmail.com>
2019-12-08 17:56:37 -0800

    updated code

--- request-handlers.js
+++ request-handlers.js
@@ -1,7 +1,7 @@
-// const exec = require('child_process').exec;
+const querystring = require('querystring');
 
-function start(response) {
-    console.log('Request handler' + start + 'was called.');
+function start(response, postData) {
+    console.log('Request handler start was called.');
 
     const body = '<html>' +
       '<head>' +
@@ -16,16 +16,16 @@ function start(response) {
  '</body>' +
  '</html>';
     
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(body);
     response.end();
 }
 
 
-function upload(response) {
-    console.log('Request handler' + upload + 'was called.');
+function upload(response, postdata) {
+    console.log('Request handler upload was called.');
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello Upload');
+    response.write('You have been test:' + querystring.parse(postdata).text);
     response.end();
 }
 
