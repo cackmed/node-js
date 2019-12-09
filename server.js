2c4f45e431b20b8e27953a09b2598b9f694949b1
Caleb Pendergraft <cackmed@gmail.com>
2019-12-08 16:46:33 -0800

    server.js written

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const http = require("http");
+const http = require('http');
 
 http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Hello World');
-    response.end()
+    response.end();
 }).listen(8888);
